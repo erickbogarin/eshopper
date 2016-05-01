8fde8e747084b66c71c68ce383e8884a48273a12
Erick Bogarin <user.email>
2016-05-01 15:02:03 -0400

    ajustes no profile

--- src/main/java/br/com/eshopper/ecommerce/conf/ServletSpringMVC.java
+++ src/main/java/br/com/eshopper/ecommerce/conf/ServletSpringMVC.java
@@ -2,22 +2,19 @@ package br.com.eshopper.ecommerce.conf;
 
 import javax.servlet.Filter;
 import javax.servlet.MultipartConfigElement;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
 
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
-import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-	@Override
+	/*@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		super.onStartup(servletContext);
 		servletContext.addListener(RequestContextListener.class);
 		servletContext.setInitParameter("spring.profiles.active", "dev");
-	}
+	}*/
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
