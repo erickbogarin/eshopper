9d133db0ecaaf63b6a0a8a75d59c50f6f779ef6e
Erick Bogarin <user.email>
2016-05-03 15:30:40 -0400

    desfazendo tentativa de migracao para o mysql

--- src/main/java/br/com/eshopper/ecommerce/conf/JPAProductionConfiguration.java
+++ src/main/java/br/com/eshopper/ecommerce/conf/JPAProductionConfiguration.java
@@ -2,6 +2,7 @@ package br.com.eshopper.ecommerce.conf;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 
@@ -17,23 +18,23 @@ public class JPAProductionConfiguration {
 	@Autowired
 	private Environment environment;
 	
-	/*@Bean
+	@Bean
 	public Properties additionalProperties() {
 		Properties props = new Properties();
 		props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 		props.setProperty("hibernate.show_sql", "true");
 		props.setProperty("hibernate.hbm2ddl.auto", "update");
 		return props;
-	}*/
+	}
 
 	@Bean
 	public DataSource dataSource() throws URISyntaxException {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setDriverClassName("org.postgresql.Driver");
 		// usuario:senha@host:port/path
-		URI dbUrl = new URI(environment.getProperty("CLEARDB_DATABASE_URL"));
+		URI dbUrl = new URI(environment.getProperty("DATABASE_URL"));
 		
-		dataSource.setUrl("jdbc:mysql://"+dbUrl.getHost()
+		dataSource.setUrl("jdbc:postgresql://"+dbUrl.getHost()
 			+":"+dbUrl.getPort()+dbUrl.getPath());
 		dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
 		dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
